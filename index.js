2057f593aef2e9d5f4ee850e7e9e710ff199a6aa
Mezan2002 <mezanurr009@gmail.com>
2022-12-24 02:08:48 +0600

    Dot env install and used

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // require start
 const express = require("express");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +14,43 @@ app.use(express.json());
 
 // initial setup start
 app.get("/", (req, res) => {
-  res.send("${website name} server is running");
+  res.send("server is running");
 });
 app.listen(port, () => {
-  console.log(`website name server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
+
+// mongo DB connect start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// collections start
+const sectorsCollection = client.db("taskHK").collection("sectors");
+// collections end
+
+// mongo DB run function start
+const run = async () => {
+  try {
+    app.get("/sectors", async (req, res) => {
+      const query = {};
+      const sectors = await sectorsCollection.find(query).toArray();
+      res.send(sectors);
+    });
+  } finally {
+    // console.log();
+  }
+};
+
+run().catch((error) => console.log(error));
+
+// mongo DB run function end
+
+// mongo DB connect end
+
 // initial setup end
